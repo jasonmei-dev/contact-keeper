bc5806f7fc1b2b92952a783b2e2d929ff56da6ab
jmei403 <jason.mei403@gmail.com>
2020-10-05 17:56:30 -0400

    Load user & set token

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import AuthContext from "./authContext";
 import AuthReducer from "./authReducer";
 import axios from "axios";
+import setAuthToken from "../../utilities/setAuthToken";
 
 import {
   REGISTER_SUCCESS,
@@ -26,6 +27,21 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
   // Load User
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get("/api/auth");
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register User
   const register = async (formData) => {
@@ -41,6 +57,8 @@ const AuthState = (props) => {
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
@@ -65,6 +83,7 @@ const AuthState = (props) => {
         user: state.user,
         error: state.error,
         register,
+        loadUser,
         clearErrors,
       }}
     >
